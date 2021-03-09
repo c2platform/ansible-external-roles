247473762056e1dd2be3c3224bb380fdf194d0e3
onknows <onno@onknows.com>
2021-03-09 09:05:15 +0100

    c2platform.tomcat with better configurable server.xml

--- c2platform.tomcat/filter_plugins/tomcat_filters.py
+++ c2platform.tomcat/filter_plugins/tomcat_filters.py
@@ -15,6 +15,8 @@ def tomcat_server_xml_properties(xml_props):
     # pprint({'ds_cmd(cmd_config)': cmd_config})
     xml = ''
     for k in xml_props:
+        if k == 'state':
+            continue
         v = xml_props[k]
         xml += '{}="{}" '.format(k, v)
     return xml
